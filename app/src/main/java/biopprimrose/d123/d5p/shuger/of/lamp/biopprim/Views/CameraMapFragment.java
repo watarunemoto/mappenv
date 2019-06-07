d979299c4191eb5c7879f0ce66d0d228c9b85561
tsuchiya_admin <13rb057@ms.dendai.ac.jp>
2019-06-08 06:05:11 +0900

    id入力の問題解決、マップ開始地点を小石川植物園

--- app/src/main/java/biopprimrose/d123/d5p/shuger/of/lamp/biopprim/Views/CameraMapFragment.java
+++ app/src/main/java/biopprimrose/d123/d5p/shuger/of/lamp/biopprim/Views/CameraMapFragment.java
@@ -1,76 +1,41 @@
 package biopprimrose.d123.d5p.shuger.of.lamp.biopprim.Views;
 
 
-import android.content.ContentValues;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.content.AsyncTaskLoader;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
-
-import com.google.android.gms.common.util.CollectionUtils;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.BitmapDescriptor;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.model.GroundOverlay;
-import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Tile;
-import com.google.android.gms.maps.model.TileOverlay;
 import com.google.android.gms.maps.model.TileOverlayOptions;
-import com.google.android.gms.maps.model.TileProvider;
-import com.google.android.gms.maps.model.UrlTileProvider;
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 import com.google.maps.android.heatmaps.HeatmapTileProvider;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Locale;
-import java.util.Map;
-import java.util.Scanner;
 import java.util.Set;
 
-import biopprimrose.d123.d5p.shuger.of.lamp.biopprim.Controllers.AsyncTaskAbstract;
-import biopprimrose.d123.d5p.shuger.of.lamp.biopprim.Controllers.Util;
 import biopprimrose.d123.d5p.shuger.of.lamp.biopprim.R;
-import biopprimrose.d123.d5p.shuger.of.lamp.biopprim.UrlCollections;
 
 import static biopprimrose.d123.d5p.shuger.of.lamp.biopprim.UrlCollections.URL_GET_LATLNG;
 
@@ -117,7 +82,8 @@ public class CameraMapFragment extends Fragment {
             @Override
             public void onMapReady(GoogleMap mMap) {
                 googleMap = mMap;
-                CameraUpdate cUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(35.985233,139.374192),18);
+                CameraUpdate cUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(35.720148,139.744347),18);
+//                CameraUpdate cUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(35.985233,139.374192),18);
                 googleMap.moveCamera(cUpdate);
 
 
