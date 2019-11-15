9e2480f1c753fb6dce1fe98ec08e5be240869845
tsuchiyadaiki <tsuchiya@MacBook-Air-6.local>
2019-11-15 17:17:16 +0900

    getdata

--- app/src/main/java/biopprimrose/d123/d5p/shuger/of/lamp/biopprim/Views/CameraMapFragment.java
+++ app/src/main/java/biopprimrose/d123/d5p/shuger/of/lamp/biopprim/Views/CameraMapFragment.java
@@ -1,8 +1,11 @@
 package biopprimrose.d123.d5p.shuger.of.lamp.biopprim.Views;
 
 
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,8 +18,26 @@ import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Set;
 
 import biopprimrose.d123.d5p.shuger.of.lamp.biopprim.R;
+import biopprimrose.d123.d5p.shuger.of.lamp.biopprim.UrlCollections;
 
 
 /**
@@ -78,151 +99,151 @@ public class CameraMapFragment extends Fragment {
 
 
                 //ヒートマップを表示させる
-//                GetData HeatMap = new GetData();
-//                HeatMap.execute();
+                GetData HeatMap = new GetData();
+                HeatMap.execute();
             }
         });
         return rootView;
     }
 
-//
-//    static class GetData extends AsyncTask<Integer,Integer, String> {
-//        @Override
-//        protected String doInBackground(Integer... value) {
-//
-//            Log.d("SampleLoader", "loadInBackground");
-//
-//            try {
-//                Thread.sleep(3000);
-//            } catch (InterruptedException ignored) {
-//
-//            }
-//
-//            int responseCode = 0;
-//            ArrayList<LatLng> responseData = null;
-//            HttpURLConnection connection = null;
-//            URL url;
-//            String urlStr = URL_GET_LATLNG.toString();
-//            Log.d(urlStr, "Urlstr");
-//            String jsontext="";
-//
-//            HashMap<String, String> queryParams = new HashMap<>();
-//            queryParams.put(mQueryKey, mQueryValue);
-//            Log.d(queryParams.toString(), "PostQueryParams");
-//            try {
-//                if (mQueryKey != null | mQueryValue != null) {
-//                    Uri.Builder builder = new Uri.Builder();
-//                    Set keys = queryParams.keySet();
-//                    for (Object key : keys) {
-//                        builder.appendQueryParameter(key.toString(), queryParams.get(key));
-//                    }
-//                    url = new URL(urlStr + builder.toString());
-//                } else {
-//                    url = new URL(urlStr);
-//                }
-//
-//
-//
-//            connection = (HttpURLConnection) url.openConnection();
-//            connection.setConnectTimeout(100000);
-//            connection.setReadTimeout(100000);
-//            connection.setRequestProperty("User-Agent", "Android");
-//            connection.setRequestProperty("Accept-Language", Locale.getDefault().toString());
-//            connection.setRequestMethod("GET");
-//            connection.setDoOutput(false);
-//            connection.setDoInput(true);
-//            connection.connect();
-//
-//
-//            InputStream in = connection.getInputStream();
-//            responseCode = connection.getResponseCode();
-//            Log.d("doInBackground", "doInBackground: "+responseCode);
-//
-//
-//            if (responseCode == 200) {
-//                    StringBuffer stringBUffer = new StringBuffer();
-//                    StringBuilder result = new StringBuilder();
-//                    InputStreamReader inReader = new InputStreamReader(in);
-//                    BufferedReader bufferedReader = new BufferedReader(inReader);
-//                    // Log.d("doinbackground", "bufferREader"+bufferedReader.readLine());
-//                    String line ;
-//                    //Log.d("result",result.toString());
-//                    while ((line = bufferedReader.readLine()) != null) {
-//                        stringBUffer.append(line);
-//                        //result.append(line);
-//                    }
-//                    //Log.d("aaaaaaa", "doInBackground: "+result);
-//                    jsontext = stringBUffer.toString();
-//                    try{
-//                        Log.d("doInBackground", "doInBackground:");
-//                        JSONArray jsonArray = new JSONObject(jsontext).getJSONArray("json");
-//                        Log.d("aaaaaaa", "doInBackground: "+jsonArray);
-//                    }catch (JSONException e){
-//
-//                    }
-//                        bufferedReader.close();
-//                        inReader.close();
-//                        in.close();
-//            }else{
-//                //Toast.makeText(getActivity(),"ヒートマップを表示できませんでした",Toast.LENGTH_SHORT).show();
-//            }
-//
-//            } catch (IOException e) {
-//             e.printStackTrace();
-//            } finally {
-//            if (connection != null) {
-//                connection.disconnect();
-//            }
-//        }
-//        //Log.d("execute", "URL:" + urlStr);
-//        //Log.d("execute", "HttpStatusCode:" + responseCode);
-//        //Log.d("execute", "ResponseData:" + responseData);
-//        //Log.d("doinbackground", "結果は"+responseData);
-//        Log.d("doInBackground", "jsontext is "+jsontext);
-//        return jsontext;
-//
-//
-//    }
-//
-//
-//    @Override
-//    protected void onPostExecute(String jsontext){
-//        ArrayList<LatLng> list = new ArrayList<LatLng>();
-//        JSONArray jsonarray =new JSONArray();
-//        Log.d("onPostExecute", jsontext);
-//            if (jsontext != "" ){
-//                try {
-//                    jsonarray = new JSONArray(jsontext);
-//                    Log.d("onPostExecute", "onPostExecute: " + jsonarray);
-//                } catch (JSONException e) {
-//
-//                }
-//
-//
-//                try {
-//                    for (int i = 0; i < jsonarray.length(); i++) {
-//                        JSONObject object = jsonarray.getJSONObject(i);
-//                        double lat = object.getDouble("latitude");
-//                        double lng = object.getDouble("longitude");
-//                        list.add(new LatLng(lat, lng));
-//                        //Log.d("latlng", "onPostExecute: " + lat + "," + lng);
-//                    }
-//                } catch (JSONException e) {
-//
-//                }
-//
-//
-//                //得られたデータからHeatMapProviderを作成
-//                Log.d("list", "onPostExecute: " + list);
-//                if (list.size()!=0){
-//                HeatmapTileProvider mProvider = new HeatmapTileProvider.Builder().data(list).build();
-//                //ヒートマップを表示させる。
-//                mOverlay = googleMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
-////                mOverlay.remove();
-//                    }
-//                }
-//        }
-//    }
+
+    static class GetData extends AsyncTask<Integer,Integer, String> {
+        @Override
+        protected String doInBackground(Integer... value) {
+
+            Log.d("SampleLoader", "loadInBackground");
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException ignored) {
+
+            }
+
+            int responseCode = 0;
+            ArrayList<LatLng> responseData = null;
+            HttpURLConnection connection = null;
+            URL url;
+            String urlStr = UrlCollections.URL_GET_LATLNG.toString();
+            Log.d(urlStr, "Urlstr");
+            String jsontext="";
+
+            HashMap<String, String> queryParams = new HashMap<>();
+            queryParams.put(mQueryKey, mQueryValue);
+            Log.d(queryParams.toString(), "PostQueryParams");
+            try {
+                if (mQueryKey != null | mQueryValue != null) {
+                    Uri.Builder builder = new Uri.Builder();
+                    Set keys = queryParams.keySet();
+                    for (Object key : keys) {
+                        builder.appendQueryParameter(key.toString(), queryParams.get(key));
+                    }
+                    url = new URL(urlStr + builder.toString());
+                } else {
+                    url = new URL(urlStr);
+                }
+
+
+
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setConnectTimeout(100000);
+            connection.setReadTimeout(100000);
+            connection.setRequestProperty("User-Agent", "Android");
+            connection.setRequestProperty("Accept-Language", Locale.getDefault().toString());
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(false);
+            connection.setDoInput(true);
+            connection.connect();
+
+
+            InputStream in = connection.getInputStream();
+            responseCode = connection.getResponseCode();
+            Log.d("doInBackground", "doInBackground: "+responseCode);
+
+
+            if (responseCode == 200) {
+                    StringBuffer stringBUffer = new StringBuffer();
+                    StringBuilder result = new StringBuilder();
+                    InputStreamReader inReader = new InputStreamReader(in);
+                    BufferedReader bufferedReader = new BufferedReader(inReader);
+                    // Log.d("doinbackground", "bufferREader"+bufferedReader.readLine());
+                    String line ;
+                    //Log.d("result",result.toString());
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBUffer.append(line);
+                        //result.append(line);
+                    }
+                    //Log.d("aaaaaaa", "doInBackground: "+result);
+                    jsontext = stringBUffer.toString();
+                    try{
+                        Log.d("doInBackground", "doInBackground:");
+                        JSONArray jsonArray = new JSONObject(jsontext).getJSONArray("json");
+                        Log.d("aaaaaaa", "doInBackground: "+jsonArray);
+                    }catch (JSONException e){
+
+                    }
+                        bufferedReader.close();
+                        inReader.close();
+                        in.close();
+            }else{
+                //Toast.makeText(getActivity(),"ヒートマップを表示できませんでした",Toast.LENGTH_SHORT).show();
+            }
+
+            } catch (IOException e) {
+             e.printStackTrace();
+            } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        //Log.d("execute", "URL:" + urlStr);
+        //Log.d("execute", "HttpStatusCode:" + responseCode);
+        //Log.d("execute", "ResponseData:" + responseData);
+        //Log.d("doinbackground", "結果は"+responseData);
+        Log.d("doInBackground", "jsontext is "+jsontext);
+        return jsontext;
+
+
+    }
+
+
+    @Override
+    protected void onPostExecute(String jsontext){
+        ArrayList<LatLng> list = new ArrayList<LatLng>();
+        JSONArray jsonarray =new JSONArray();
+        Log.d("onPostExecute", jsontext);
+            if (jsontext != "" ){
+                try {
+                    jsonarray = new JSONArray(jsontext);
+                    Log.d("onPostExecute", "onPostExecute: " + jsonarray);
+                } catch (JSONException e) {
+
+                }
+
+
+                try {
+                    for (int i = 0; i < jsonarray.length(); i++) {
+                        JSONObject object = jsonarray.getJSONObject(i);
+                        double lat = object.getDouble("latitude");
+                        double lng = object.getDouble("longitude");
+                        list.add(new LatLng(lat, lng));
+                        //Log.d("latlng", "onPostExecute: " + lat + "," + lng);
+                    }
+                } catch (JSONException e) {
+
+                }
+
+
+                //得られたデータからHeatMapProviderを作成
+                Log.d("list", "onPostExecute: " + list);
+                if (list.size()!=0){
+                HeatmapTileProvider mProvider = new HeatmapTileProvider.Builder().data(list).build();
+                //ヒートマップを表示させる。
+                mOverlay = googleMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+//                mOverlay.remove();
+                    }
+                }
+        }
+    }
 
 
 
