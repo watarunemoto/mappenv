8a27bca5625a92ec8f94ee938408635a74c75f50
15rb034 <15rb034@ms.dendai.acjp>
2020-07-14 16:05:27 +0900

    イベント情報をrecyclerView表示

--- app/src/main/java/biopprimrose/d123/d5p/shuger/of/lamp/biopprim/Views/EventContentsActivity.java
+++ app/src/main/java/biopprimrose/d123/d5p/shuger/of/lamp/biopprim/Views/EventContentsActivity.java
@@ -14,6 +14,9 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -34,8 +37,12 @@ import biopprimrose.d123.d5p.shuger.of.lamp.biopprim.R;
 import biopprimrose.d123.d5p.shuger.of.lamp.biopprim.UrlCollections;
 
 public class EventContentsActivity extends AppCompatActivity {
-
-
+    //イベント情報リストにおけるイベント名のキー値
+    String key1 = "eventname";
+    //イベント情報リストにおける主催者のキー値
+    String key2 = "organizer";
+    //イベント情報リストにおけるコンテンツ内容のキー値
+    String key3 = "eventscontents";
 
 
 
@@ -48,130 +55,11 @@ public class EventContentsActivity extends AppCompatActivity {
         EventInfoReceiver receiver = new EventInfoReceiver();
         //EventInfoReceiverを実行
         receiver.execute();
-/**
-        //RecyclerViewを取得
-        RecyclerView lvMenu = findViewById(R.id.MyRecyclerView);
-        //LinearLayoutManagerオブジェクトを生成
-        LinearLayoutManager layout = new LinearLayoutManager(EventContentsActivity.this);
-        //RecyclerViewにレイアウトマネージャーとしてLinearLayoutManagerを設定
-        lvMenu.setLayoutManager(layout);
-        //メニューリストデータを生成
-        List<Map<String,Object>> menuList = createTeishokuList();
-        //アダプタオブジェクトを生成
-        RecyclerListAdapter adapter = new RecyclerListAdapter(menuList);
-        //RecyclearViewにアダプタオブジェクトを設定。
-        lvMenu.setAdapter(adapter);
-        **/
     }
-
-
-
-
-
-
-
-
-    private class RecyclerListViewHolder extends RecyclerView.ViewHolder {
-        //リスト一行分でメニュー名を表示する画面部品
-        public TextView _tvMenuName;
-        //リスト一行分で金額を表示する画面部品
-        public TextView _tvMenuPrice;
-
-        /**
-         * コンストラクタ
-         * @param itemView　リスト一行分の画面部品
-         */
-        public RecyclerListViewHolder(View itemView) {
-            //親クラスのコンストラクタの呼び出し
-            super(itemView);
-            //引数で渡されたリスト一行分の画面部品中から表示に使われるTextViewを取得
-            _tvMenuName = itemView.findViewById(R.id.tvMenuName);
-            _tvMenuPrice = itemView.findViewById(R.id.tvMenuPrice);
-        }
-    }
-
-
-    private class RecyclerListAdapter extends RecyclerView.Adapter<RecyclerListViewHolder> {
-        //リストデータを保持するフィールド
-        private List<Map<String, Object>> _listData;
-
-        /**
-         * コンストラクタ
-         * @param listData
-         */
-        public RecyclerListAdapter(List<Map<String, Object>> listData) {
-            //引数のリストデータをフィールドに格納
-            _listData = listData;
-        }
-
-
-    @Override
-    public RecyclerListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        //レイアウトインフレーターを取得
-        LayoutInflater inflater = LayoutInflater.from(EventContentsActivity.this);
-        //my_text_viewをインフレートし、一行分の画面部品とする。
-        View view = inflater.inflate(R.layout.my_text_view, parent, false);
-        //ビューホルダーを生成
-        RecyclerListViewHolder holder = new RecyclerListViewHolder(view);
-        //生成したビューホルダーを返す。
-        return holder;
-    }
-
-    @Override
-    public void onBindViewHolder(RecyclerListViewHolder holder, int position) {
-        //リストデータから該当一行分のデータを取得
-        Map<String, Object> item = _listData.get(position);
-        //メニュー名文字列を取得
-        String menuName = (String) item.get("name");
-        Log.d("aiuto",menuName);
-        //メニュー金額を取得
-        int menuPrice = (int) item.get("price");
-        //金額を文字列に変換
-        String menuPriceStr = String.valueOf(menuPrice);
-        Log.d("aiueo",menuPriceStr);
-        //メニュー名と金額をビューホルダー中のTextViewに設定。
-        holder._tvMenuName.setText(menuName);
-        holder._tvMenuPrice.setText(menuPriceStr);
-    }
-
-    @Override
-    public int getItemCount() {
-        //リストデータ中の件数をリターン
-        return _listData.size();
-    }
-
-}
-
-
-//RecyclerViewに設定するリストデータを用意
-
-    private List<Map<String,Object>> createTeishokuList(){
-        List<Map<String,Object>> menuList = new ArrayList<>();
-
-        Map<String,Object> menu = new HashMap<>();
-        menu.put("name","My name is hoge");
-        menu.put("price", 700);
-        menuList.add(menu);
-
-        menu = new HashMap<>();
-        menu.put("name", "Hello");
-        menu.put("price",500);
-        menuList.add(menu);
-
-
-        return menuList;
-
-    }
-
-
-
 
 
     //非同期処理のクラス
-
     private class EventInfoReceiver extends AsyncTask<String,String,String> {
-
-
         @Override
         public String doInBackground(String...params){
             //接続URLを作成
@@ -213,65 +101,43 @@ public class EventContentsActivity extends AppCompatActivity {
                         is.close();
                     }
                     catch (IOException ex){
-
                     }
                 }
             }
-
             //Json文字列を返す
             return result;
         }
 
 
-        //
+        //HTTP接続後、得られたJSON文字列からrecyclerViewを設定
         @Override
         public void onPostExecute(String result){
-            //JSON文字列を解析、List<Map<String,Object>>の形にする。
             try {
-                //JSON文字列からJSONObjectオブジェクトを作成。これをルートJSONオブジェクトとする。
-                //JSONObject rootJSON = new JSONObject(result);
+                //取得できたJSON文字列を、リストの形に成形する。
                 ObjectMapper mapper = new ObjectMapper();
-                TypeReference<List<String,Object>> type = new TypeRecerence<List<Map<String,Object>>>(){}
-                List<Map<String,Object>> list = mapper.readValue(result,type);
+                TypeReference<List<Map<String,Object>>> type = new TypeReference<List<Map<String, Object>>>(){};
+                List<Map<String,Object>> eventlist = mapper.readValue(result,type);
 
-
-
-                /**
-                //ルートJSON直下の「description」JSONオブジェクトを取得
-                JSONObject descriptionJSON = rootJSON.getJSONObject("description");
-                //「description」プロパティ直下の「text」文字列を取得
-                desc = descriptionJSON.getString("text");
-                //ルートJSON直下の「forecasts」JSON配列を取得
-                JSONArray forecasts = rootJSON.getJSONArray("forecasts");
-                //「forecasts」JSON配列の一つ目のJSONobjectを取得
-                JSONObject forecastNow = forecasts.getJSONObject(0);
-                //「forecasts」一つ目のJSONオブジェクトから「telop」文字列を取得
-                telop = forecastNow.getString("telop");
-                 **/
+                //RecyclerViewを取得
+                RecyclerView lvMenu = findViewById(R.id.MyRecyclerView);
+                //LinearLayoutManagerオブジェクトを生成
+                LinearLayoutManager layout = new LinearLayoutManager(EventContentsActivity.this);
+                //RecyclerViewにレイアウトマネージャーとしてLinearLayoutManagerを設定
+                lvMenu.setLayoutManager(layout);
+                //アダプタオブジェクトを生成
+                RecyclerListAdapter adapter = new RecyclerListAdapter(eventlist);
+                //RecyclearViewにアダプタオブジェクトを設定。
+                lvMenu.setAdapter(adapter);
             }
-            catch (JSONException ex){
+            catch (IOException ex){
             }
-            /**
-            //RecyclerViewを取得
-            RecyclerView lvMenu = findViewById(R.id.MyRecyclerView);
-            //LinearLayoutManagerオブジェクトを生成
-            LinearLayoutManager layout = new LinearLayoutManager(EventContentsActivity.this);
-            //RecyclerViewにレイアウトマネージャーとしてLinearLayoutManagerを設定
-            lvMenu.setLayoutManager(layout);
-            //メニューリストデータを生成
-            List<Map<String,Object>> menuList = createTeishokuList();
-            //アダプタオブジェクトを生成
-            RecyclerListAdapter adapter = new RecyclerListAdapter(menuList);
-            //RecyclearViewにアダプタオブジェクトを設定。
-            lvMenu.setAdapter(adapter);
-             **/
-            Log.d("aiueo",result);
+            //Log.d("aiueo",result);
         }
 
 
         //取得したInputStreamを文字列に変形するメソッド
         private String is2String(InputStream is) throws IOException{
-            BufferedReader reader = new BufferedReader(new InputStreamReader(is,"UTF-8"))
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is,"UTF-8"));
             StringBuffer sb = new StringBuffer();
             char[] b = new char[1024];
             int line;
@@ -282,6 +148,83 @@ public class EventContentsActivity extends AppCompatActivity {
 
         }
     }
+
+
+
+
+
+
+
+    private class RecyclerListViewHolder extends RecyclerView.ViewHolder {
+        //リスト一行分でメニュー名を表示する画面部品
+        public TextView _tvMenuName;
+        //リスト一行分で金額を表示する画面部品
+        public TextView _tvMenuPrice;
+
+        /**
+         * コンストラクタ
+         * @param itemView　リスト一行分の画面部品
+         */
+        public RecyclerListViewHolder(View itemView) {
+            //親クラスのコンストラクタの呼び出し
+            super(itemView);
+            //引数で渡されたリスト一行分の画面部品中から表示に使われるTextViewを取得
+            _tvMenuName = itemView.findViewById(R.id.tvMenuName);
+            _tvMenuPrice = itemView.findViewById(R.id.tvMenuPrice);
+        }
+    }
+
+
+
+
+    private class RecyclerListAdapter extends RecyclerView.Adapter<RecyclerListViewHolder> {
+        //リストデータを保持するフィールド
+        private List<Map<String, Object>> _listData;
+
+        /**
+         * コンストラクタ
+         * @param listData
+         */
+        public RecyclerListAdapter(List<Map<String, Object>> listData) {
+            //引数のリストデータをフィールドに格納
+            _listData = listData;
+        }
+
+
+        @Override
+        public RecyclerListViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            //レイアウトインフレーターを取得
+            LayoutInflater inflater = LayoutInflater.from(EventContentsActivity.this);
+            //my_text_viewをインフレートし、一行分の画面部品とする。
+            View view = inflater.inflate(R.layout.my_text_view, parent, false);
+            //ビューホルダーを生成
+            RecyclerListViewHolder holder = new RecyclerListViewHolder(view);
+            //生成したビューホルダーを返す。
+            return holder;
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerListViewHolder holder, int position) {
+            //リストデータから該当一行分のデータを取得
+            Map<String, Object> item = _listData.get(position);
+            //”eventname”の文字列を取得
+            String menuName = (String) item.get(key1);
+            //"organizer"の文字列を取得
+            String menuPrice = (String) item.get(key2);
+            //金額を文字列に変換
+            String menuPriceStr = String.valueOf(menuPrice);
+            //メニュー名と金額をビューホルダー中のTextViewに設定。
+            holder._tvMenuName.setText(menuName);
+            holder._tvMenuPrice.setText(menuPriceStr);
+        }
+
+        @Override
+        public int getItemCount() {
+            //リストデータ中の件数をリターン
+            return _listData.size();
+        }
+    }
+
 }
 
 
